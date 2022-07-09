48b29c72b5f50fe78a0e1543ff5b73519415a53b
Tomoki Nakamaru <tomoki.nakamaru@gmail.com>
2022-07-09 12:15:35 +0900

    Add utils

---
+++ xptools/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+from .utils import shell
+from .utils import xpfix
---
+++ xptools/utils.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from subprocess import run
+from subprocess import PIPE
+from subprocess import STDOUT
+
+
+def shell(*args, **kwargs):
+    command = ' '.join((
+        *(f'{k}={v}' for k, v in kwargs.items()),
+        *map(str, args)
+    ))
+    print(f'[shell] {command}')
+    process = run(
+        command, shell=True, text=True,
+        stdout=PIPE, stderr=STDOUT
+    )
+    if process.returncode:
+        output = process.stdout.strip()
+        output and print(output)
+        exit(1)
+
+
+def xpfix(src, dst=None, **kwargs):
+    src = Path(src)
+    dst = dst or src.parent / f'.{src.name}'
+    shell(
+        'xpfix',
+        *(f"'{k}={v}'" for k, v in kwargs.items()),
+        '<', src, '>', dst
+    )
+    return dst
